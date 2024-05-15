7084de2e8b0493bc22f8929ffcb8ec9785c56067
Williams Barriquero <williamsgomess@outlook.com>
2024-05-15 17:11:04 -0300

    Entidade Category
    Escrevendo testes parte 1
    Escrevendo testes parte 2

---
+++ README.md
@@ -0,0 +1,11 @@
+# codeflix-catalog-admin
+Administração de catálogo de vídeos - CodeFlix - Python
+
+## Regras Category
+- [x] Nome da categoria deverá ser obrigatório
+- [x] Nome deverá ter no máximo 255 caracteres
+- [x] Ao criar uma nova categoria um identificador deverá ser gerado no formato (uuid).
+- [x] Ao cria uma categoria os campos id, description e isActive não serão obrigatórios.
+- [x] Caso a propriedade isActive não seja informada, deverá ser marcada como true.
+- [ ] Teste __str__ - Exercício
+- [ ] Teste __repr__ - Exercício
--- test_category.py
+++ test_category.py
@@ -1,4 +1,6 @@
 import unittest
+import uuid
+from uuid import UUID
 
 from category import Category
 
@@ -11,6 +13,33 @@ class TestCategory(unittest.TestCase):
     def test_name_must_have_less_or_equal_than_255_characters(self):
         with self.assertRaisesRegex(ValueError, "name must have less or equal than 255 characters"):
             Category("a" * 256)
+
+    def test_category_must_be_created_with_id_as_uuid_by_default(self):
+        category = Category(name="Films")
+        self.assertEqual(type(category.id), UUID)
+
+    def test_created_category_with_default_values(self):
+        category = Category(name="Films")
+        self.assertEqual(category.name, "Films")
+        self.assertEqual(category.description, "")
+        self.assertEqual(category.is_active, True)
+
+    def test_category_is_created_as_active_by_default(self):
+        category = Category(name="Films")
+        self.assertEqual(category.is_active, True)
+
+    def test_category_is_created_with_provided_values(self):
+        category_id = uuid.uuid4()
+        category = Category(
+            id=category_id,
+            name="Films",
+            description="Movies",
+            is_active=False
+        )
+        self.assertEqual(category.id, category_id)
+        self.assertEqual(category.name, "Films")
+        self.assertEqual(category.description, "Movies")
+        self.assertEqual(category.is_active, False)
 
 
 if __name__ == "__main__":
