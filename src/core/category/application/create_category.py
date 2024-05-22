fbd9f30dab66a5340f071298afe8e3b543969768
Williams Barriquero <williamsgomess@outlook.com>
2024-05-22 17:36:48 -0300

    feat: Refactor create_category function to use dataclasses and handle invalid category data
    Refactor the `create_category` function in `create_category.py` to use dataclasses for improved readability and maintainability. Additionally, handle invalid category data by raising an `InvalidCategoryData` exception when the `name` parameter is empty. This ensures that only valid category data is used to create new categories in the application.

--- src/core/category/application/create_category.py
+++ src/core/category/application/create_category.py
@@ -1,28 +1,37 @@
+from dataclasses import dataclass
 from uuid import UUID
 
+from src.core.category.application.exceptions import InvalidCategoryData
 from src.core.category.domain.category import Category
 from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
 
 
-class InvalidCategoryData(Exception):
-    pass
-
-
-def create_category(
-    repository: InMemoryCategoryRepository,
-    name: str,
-    description: str = "",
+@dataclass
+class CreateCategoryResquest:
+    name: str
+    description: str = ""
     is_active: bool = True
-) -> UUID:
-    try:
-        category = Category(
-            name=name,
-            description=description,
-            is_active=is_active
-        )
-    except ValueError as err:
-        raise InvalidCategoryData(err)
 
-    repository.save(category)
 
-    return category.id
+@dataclass
+class CreateCategoryResponse:
+    id: UUID
+
+
+class CreateCategory:
+    def __init__(self, repository: InMemoryCategoryRepository):
+        self.repository = repository
+
+    def execute(self, request: CreateCategoryResquest) -> CreateCategoryResponse:
+        try:
+            category = Category(
+                name=request.name,
+                description=request.description,
+                is_active=request.is_active
+            )
+        except ValueError as err:
+            raise InvalidCategoryData(err)
+
+        self.repository.save(category)
+
+        return category.id
--- src/core/category/tests/application/test_create_category.py
+++ src/core/category/tests/application/test_create_category.py
@@ -3,31 +3,33 @@ from uuid import UUID
 
 import pytest
 
-from src.core.category.application.create_category import InvalidCategoryData, create_category
+from src.core.category.application.create_category import CreateCategory, CreateCategoryResquest
+from src.core.category.application.exceptions import InvalidCategoryData
 from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
 
 
 class TestCreateCategory:
     def test_create_category_with_valid_data(self):
         mock_repository = MagicMock(InMemoryCategoryRepository)
-        category_id = create_category(
-            repository=mock_repository,
+        use_case = CreateCategory(repository=mock_repository)
+        request = CreateCategoryResquest(
             name="Films",
             description="Films about action",
-            is_active=True
+            is_active=True  # default value
         )
+
+        category_id = use_case.execute(request)
 
         assert category_id is not None
         assert isinstance(category_id, UUID)
         assert mock_repository.save.called is True
 
     def test_create_category_with_invalid_data(self):
-        mock_repository = MagicMock(InMemoryCategoryRepository)
+        use_case = CreateCategory(
+            repository=MagicMock(InMemoryCategoryRepository))
+
         with pytest.raises(InvalidCategoryData, match="name cannot be empty") as exc_info:
-            create_category(
-                repository=mock_repository,
-                name="",
-                description=""
-            )
+            use_case.execute(CreateCategoryResquest(name=""))
+
         assert exc_info.type is InvalidCategoryData
         assert str(exc_info.value) == "name cannot be empty"
