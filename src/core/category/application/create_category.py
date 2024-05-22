10bbb735c15b362c7a20221fd252c68558bc5326
Williams Barriquero <williamsgomess@outlook.com>
2024-05-22 14:08:07 -0300

    feat: Handle invalid category data in create_category function
    The `create_category` function in `create_category.py` now handles invalid category data by raising an `InvalidCategoryData` exception when the `name` parameter is empty. This ensures that only valid category data is used to create new categories in the application.

--- src/core/category/application/create_category.py
+++ src/core/category/application/create_category.py
@@ -3,7 +3,18 @@ from uuid import UUID
 from src.core.category.domain.category import Category
 
 
-def create_category(name: str, description: str, is_active: bool = True) -> UUID:
-    category = Category(name=name, description=description, is_active=is_active)
+class InvalidCategoryData(Exception):
+    pass
+
+
+def create_category(name: str, description: str = "", is_active: bool = True) -> UUID:
+    try:
+        category = Category(
+            name=name,
+            description=description,
+            is_active=is_active
+        )
+    except ValueError as err:
+        raise InvalidCategoryData(err)
 
     return category.id
--- src/core/category/tests/application/test_create_category.py
+++ src/core/category/tests/application/test_create_category.py
@@ -1,13 +1,26 @@
 from uuid import UUID
 
-from src.core.category.application.create_category import create_category
+import pytest
+
+from src.core.category.application.create_category import InvalidCategoryData, create_category
 
 
 class TestCreateCategory:
     def test_create_category_with_valid_data(self):
         category_id = create_category(
-            name="Films", description="Films about action", is_active=True
+            name="Films",
+            description="Films about action",
+            is_active=True
         )
 
         assert category_id is not None
         assert isinstance(category_id, UUID)
+
+    def test_create_category_with_invalid_data(self):
+        with pytest.raises(InvalidCategoryData, match="name cannot be empty") as exc_info:
+            create_category(
+                name="",
+                description=""
+            )
+        assert exc_info.type is InvalidCategoryData
+        assert str(exc_info.value) == "name cannot be empty"
