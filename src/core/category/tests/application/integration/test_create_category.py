8055588441180e42968eec5f0b9bc9f2fae54eba
Williams Barriquero <williamsgomess@outlook.com>
2024-05-23 00:25:46 -0300

    feat: Refactor unit tests and add integration tests for category creation
    The unit test for creating category in 'test_create_category.py' has been refactored for clarity. A separate integration test for the same functionality has also been created in a new file 'test_create_category.py'. This enhances the test coverage, ensuring that the creation of categories is working both at the unit and system level.

---
+++ src/core/category/tests/application/integration/test_create_category.py
@@ -0,0 +1,28 @@
+from uuid import UUID
+
+from src.core.category.application.create_category import CreateCategory, CreateCategoryRequest
+from src.core.category.infra.in_memory_category_repository import InMemoryCategoryRepository
+
+
+class TestCreateCategory:
+    def test_create_category_with_valid_data(self):
+        repository = InMemoryCategoryRepository()
+        use_case = CreateCategory(repository=repository)
+        request = CreateCategoryRequest(
+            name="Film",
+            description="Film description",
+            is_active=True,  # default
+        )
+
+        response = use_case.execute(request)
+
+        assert response.id is not None
+        assert isinstance(response.id, UUID)
+        assert len(repository.categories) == 1
+        assert repository.categories[0].id == response.id
+
+        persisted_category = repository.categories[0]
+        assert persisted_category.id == response.id
+        assert persisted_category.name == "Film"
+        assert persisted_category.description == "Film description"
+        assert persisted_category.is_active == True
--- src/core/category/tests/application/unit/test_create_category.py
+++ src/core/category/tests/application/unit/test_create_category.py
@@ -18,10 +18,10 @@ class TestCreateCategory:
             is_active=True  # default value
         )
 
-        category_id = use_case.execute(request).id
+        response = use_case.execute(request)
 
-        assert category_id is not None
-        assert isinstance(category_id, UUID)
+        assert response.id is not None
+        assert isinstance(response.id, UUID)
         assert mock_repository.save.called is True
 
     def test_create_category_with_invalid_data(self):
